1ae27a0e91dc319ebdd2bc796e635b463368fe30
Prajjwal Pratap Singh <prajjwalpratap110@gmail.com>
2025-10-24 23:01:46 +0530

    adding some middlewares

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const homeRoutes = require('./routes/home.routes');
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api/auth',authRoutes);
-app.use('/api/upload',homeRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/upload', homeRoutes);
 
 module.exports = app;
--- Backend/src/routes/home.routes.js
+++ Backend/src/routes/home.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const  uploadController  = require("../controllers/upload.controller")
 const multer = require("multer");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/form-submission", upload.single("video"), uploadController)
+router.post("/form-submission", authMiddleware, upload.single("video"), uploadController)
 
 module.exports = router;
